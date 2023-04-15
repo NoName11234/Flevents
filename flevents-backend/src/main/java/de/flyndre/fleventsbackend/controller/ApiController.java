6bf2eaccdf29f6d20d6b783521149dedea512797
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2023-04-15 10:19:09 +0200

    added ApiService

--- flevents-backend/src/main/java/de/flyndre/fleventsbackend/controller/ApiController.java
+++ flevents-backend/src/main/java/de/flyndre/fleventsbackend/controller/ApiController.java
@@ -3,6 +3,8 @@ package de.flyndre.fleventsbackend.controller;
 import de.flyndre.fleventsbackend.Models.*;
 import de.flyndre.fleventsbackend.Models.Event;
 import de.flyndre.fleventsbackend.repositories.*;
+import de.flyndre.fleventsbackend.services.ApiService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +26,8 @@ public class ApiController {
     private OrganizationAccountRepository organizationAccountRepository;
     private EventRegistrationRepository eventRegistrationRepository;
     private EventRepository eventRepository;
+    @Autowired
+    private ApiService apiService;
 
     public ApiController(OrganizationRepository organizationRepository, FleventsAccountRepository fleventsAccountRepository, OrganizationAccountRepository organizationAccountRepository, EventRegistrationRepository eventRegistrationRepository, EventRepository eventRepository) {
         this.organizationRepository = organizationRepository;
@@ -39,40 +43,7 @@ public class ApiController {
     }
 
     @GetMapping("/initdb")
-    public FleventsAccount initDB() throws IOException {
-        Organization flyndre = new Organization(null,"Flyndre","the best organization of the world",null,null,null,null,null);
-        flyndre = organizationRepository.save(flyndre);
-        Organization dhbw = new Organization(null,"DHBW","not so the best university of the world",null,null,null,null,null);
-        dhbw = organizationRepository.save(dhbw);
-
-        FleventsAccount lukas = fleventsAccountRepository.save(new FleventsAccount(null,"Lukas","Burkhardt","i21005@hb.dhbw-stuttgart.de",null,"You'll never gona gues this!!",null,null));
-        FleventsAccount paul = fleventsAccountRepository.save(new FleventsAccount(null,"Paul","Lehmann","paul@hb.dhbw-stuttgart.de",null,"You'll never gonna gues this!!",null,null));
-        FleventsAccount ruben = fleventsAccountRepository.save(new FleventsAccount(null,"Ruben","Kraft","ruben@hb.dhbw-stuttgart.de",null,"You'll never gonna guess this!!",null,null));
-        FleventsAccount pasi = fleventsAccountRepository.save(new FleventsAccount(null,"Pascal","Fuchs","pasi@hb.dhbw-stuttgart.de",null,"You'll gonna guess this!!",null,null));
-        FleventsAccount david = fleventsAccountRepository.save(new FleventsAccount(null, "David", "Maier", "dm@flyndre.de", null, "wireshork", null, null));
-
-        //Event birthday = eventRepository.save(new Event(null,"Birthday","This is my birthday party",null,null,null,"My house",null,flyndre,null));
-        //Event exam = eventRepository.save(new Event(null,"Exam","This is the exam from the lecture swe",null,null,null,"Campus Horb",null,dhbw,null));
-
-
-        organizationAccountRepository.save(new OrganizationAccount(null,flyndre,lukas,OrganizationRole.admin));
-        organizationAccountRepository.save(new OrganizationAccount(null,flyndre,paul,OrganizationRole.member));
-        organizationAccountRepository.save(new OrganizationAccount(null,flyndre,ruben,OrganizationRole.organizer));
-        organizationAccountRepository.save(new OrganizationAccount(null,flyndre,david,OrganizationRole.admin));
-        organizationAccountRepository.save(new OrganizationAccount(null,dhbw,lukas,OrganizationRole.admin));
-        organizationAccountRepository.save(new OrganizationAccount(null,dhbw,paul,OrganizationRole.member));
-        organizationAccountRepository.save(new OrganizationAccount(null,dhbw,david,OrganizationRole.admin));
-
-        //eventRegistrationRepository.save(new EventRegistration(null,birthday,lukas,EventRole.organizer));
-        //eventRegistrationRepository.save(new EventRegistration(null,birthday,paul,EventRole.attendee));
-        //eventRegistrationRepository.save(new EventRegistration(null,birthday,ruben,EventRole.guest));
-        //eventRegistrationRepository.save(new EventRegistration(null,birthday,pasi,EventRole.invited));
-        //eventRegistrationRepository.save(new EventRegistration(null,exam,paul,EventRole.organizer));
-        //eventRegistrationRepository.save(new EventRegistration(null,exam,lukas,EventRole.tutor));
-        //eventRegistrationRepository.save(new EventRegistration(null,exam,ruben,EventRole.attendee));
-        //eventRegistrationRepository.save(new EventRegistration(null,exam,david,EventRole.attendee));
-
-
-        return lukas;
+    public void initDB() throws IOException {
+        apiService.initDB();
     }
 }
