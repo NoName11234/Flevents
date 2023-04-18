c0fa3b87961f020020bcedb25d4fd85d4329d2ca
dasdawidt <David_Maier@gmx.de>
2023-04-18 17:44:50 +0200

    update: update post data model to match the backend's

--- flevents-frontend/src/models/post.ts
+++ flevents-frontend/src/models/post.ts
@@ -1,12 +1,16 @@
 import {Account} from "@/models/account";
+import {FleventsEventPreview} from "@/models/fleventsEventPreview";
+import {Comment} from "@/models/comment";
 
 export interface Post {
 
   uuid: string,
   author: Account,
   title: string,
-  text: string,
-  date: Date,
+  content: string,
+  event: FleventsEventPreview,
+  creationDate: Date,
   attachments: Array<string>,
+  comments: Array<Comment>,
 
 }
