c6c7fe23350e4e1a8758ff2dc165e95386d4363d
dasdawidt <David_Maier@gmx.de>
2023-04-18 20:39:23 +0200

    feat: create account avatar component

---
+++ flevents-frontend/src/components/AccountAvatar.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import {Account} from "@/models/account";
+import ColorService from "@/service/colorService";
+
+const defaultAvatarSize = 80;
+const iconSizeMultiplier = 0.625;
+
+defineProps({
+  account: {
+    required: true,
+    type: Object as () => Account,
+  },
+  size: {
+    required: false,
+    type: String||Number,
+  },
+});
+</script>
+
+<template>
+  <v-avatar
+    class="flex-grow-0"
+    :color="ColorService.getAvatarColor(account)"
+    :size="size || defaultAvatarSize"
+  >
+    <v-icon
+      icon="mdi-account"
+      :size="(size || defaultAvatarSize) * iconSizeMultiplier"
+    />
+  </v-avatar>
+</template>
+
+<style scoped>
+
+</style>
