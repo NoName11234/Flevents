798cb43930dade38f550c96af84cd2534f036f9a
dasdawidt <David_Maier@gmx.de>
2023-04-18 20:40:40 +0200

    refactor: use color generation service

--- flevents-frontend/src/components/AccountInfo.vue
+++ flevents-frontend/src/components/AccountInfo.vue
@@ -1,13 +1,22 @@
+<script lang="ts">
+import { Account } from '@/models/account'
+import AccountAvatar from "@/components/AccountAvatar.vue";
+export default {
+  name: "AccountInfo",
+  components: {AccountAvatar},
+  props: {
+    account: {
+      required: true,
+      type: Object as () => Account,
+    }
+  }
+}
+</script>
+
 <template>
   <v-card class="pa-5">
   <div class="d-flex flex-row" style="gap: 20px">
-      <v-avatar
-        class="flex-grow-0"
-        :color="`rgb(${account.email.charCodeAt(0)*2},${account.firstname.charCodeAt(0)*2},${account.lastname.charCodeAt(0)*2})`"
-        size="80"
-      >
-        <v-icon icon="mdi-account" size="50"></v-icon>
-      </v-avatar>
+      <AccountAvatar :account="account" />
       <div class="d-flex flex-column justify-center align-start flex-grow-1">
         <h2>{{account.firstname}} {{account.lastname}}</h2>
         <span>{{account.email}}</span>
@@ -15,19 +24,6 @@
   </div>
   </v-card>
 </template>
-
-<script lang="ts">
-import { Account } from '@/models/account'
-export default {
-  name: "AccountInfo",
-  props: {
-    account: {
-        required: true,
-        type: Object as () => Account,
-    }
-  }
-}
-</script>
 
 <style scoped>
 </style>
