b302ccdc0d691f91a91e99bf29d544a51e90e7fd
dasdawidt <David_Maier@gmx.de>
2023-04-18 15:17:58 +0200

    feat: create event service

---
+++ flevents-frontend/src/service/eventService.ts
@@ -0,0 +1,53 @@
+import api from "@/api";
+import {FleventsEvent} from "@/models/fleventsEvent";
+import {EventRole} from "@/models/eventRole";
+
+const base = `/events`
+
+class EventService {
+
+  // CRUD
+  create(event: FleventsEvent) {
+    return api.post(`${base}`, event);
+  }
+  get(uuid: string) {
+    return api.get(`${base}/${uuid}`);
+  }
+  edit(uuid: string, event: FleventsEvent) {
+    return api.put(`${base}/${uuid}`, event);
+  }
+  delete(uuid: string) {
+    return api.delete(`${base}/${uuid}`);
+  }
+
+  // Organizer-Management
+  getOrganizers(uuid: string) {
+    return api.get(`${base}/${uuid}/organizers`);
+  }
+  addOrganizer(uuid: string, accountUuid: string, token: string) {
+    return api.post(`${base}/${uuid}/organizers`, {}, {
+      params: {
+        accountId: accountUuid,
+        token,
+      }
+    });
+  }
+  removeOrganizer(uuid: string, accountUuid: string) {
+    return api.delete(`${base}/organizers/${accountUuid}`);
+  }
+
+  // Attendee-Management
+  getAttendees(uuid: string) {
+    return api.get(`${base}/${uuid}/attendees`);
+  }
+  inviteAttendee(uuid: string, accountUuid: string, role: EventRole.attendee|EventRole.tutor) {
+    return api.post(`${base}/${uuid}/invite`, {}, {
+      params: {
+        accountId: accountUuid,
+        role: EventRole.organizer,
+      }
+    });
+  }
+}
+
+export default new EventService();
