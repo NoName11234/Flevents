d59c8aef5b5f8c160d18464e918f0298de8ac507
dasdawidt <David_Maier@gmx.de>
2023-04-20 01:48:24 +0200

    fix: change api path to localhost

--- flevents-frontend/src/api.ts
+++ flevents-frontend/src/api.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 
 
 const properties = {
-  url: 'http://h3005487.stratoserver.net:8082/api',
+  // url: 'http://h3005487.stratoserver.net:8082/api',
+  url: 'http://localhost:8082/api',
 }
 
 const api = axios.create({
