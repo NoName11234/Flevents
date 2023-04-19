b172f4d9a98dad93db3cda6df1d6b2fdf9c7f385
dasdawidt <David_Maier@gmx.de>
2023-04-20 01:48:48 +0200

    feat: properly define pinia event-store

--- flevents-frontend/src/store/events.ts
+++ flevents-frontend/src/store/events.ts
@@ -1,26 +1,49 @@
 // Utilities
 import { defineStore } from 'pinia'
-import axios from "axios";
+import AccountService from "@/service/accountService";
+import Security from "@/service/security";
+import {FleventsEvent} from "@/models/fleventsEvent";
 
 export const useEventStore = defineStore('events', {
   state: () => ({
-    events: [],
+    bookedEvents: [] as FleventsEvent[],
+    managedEvents: [] as FleventsEvent[],
+    exploreEvents: [] as FleventsEvent[],
+    loading: false,
+    error: false,
   }),
   actions: {
-    async fetchEvents() {
-      const api = 'http://h3005487.stratoserver.net:8082/api/events';
+    async hydrate() {
+      this.error = false;
+      this.loading = true;
+      const account = Security.getAccount();
+      if (account === null) {
+        console.error('Cannot get events without logged-in user.');
+        this.error = true;
+        this.loading = false;
+        return;
+      }
       try {
-        const response = await axios.get(api);
-        this.events = response.data;
-        console.log(`Fetched events: ${JSON.stringify(this.events)}`);
-      } catch (e) {
-        console.log(e)
+        const responses = await Promise.all([
+          AccountService.getBookedEvents(account.uuid),
+          AccountService.getManagedEvents(account.uuid),
+          AccountService.getExploreEvents(account.uuid)
+        ]);
+        console.log(responses);
+        this.bookedEvents = responses[0].data as FleventsEvent[];
+        this.managedEvents = responses[1].data as FleventsEvent[];
+        this.exploreEvents = responses[2].data as FleventsEvent[];
+      } catch (error) {
+        console.warn('Failed to fetch events of logged-in user.');
+        this.error = true;
+      } finally {
+        this.loading = false;
       }
     },
+    getEvent(uuid: string) {
+      return this.bookedEvents
+        .concat(this.managedEvents, this.exploreEvents)
+        .find(e => e.uuid === uuid);
+    },
   },
-  getters: {
-    getEvents(state) {
-      return state.events;
-    }
-  }
 })
