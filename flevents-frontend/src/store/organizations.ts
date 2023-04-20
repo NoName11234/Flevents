a2bf7d719d6aa013380dcb5d9b465869e50b473a
dasdawidt <David_Maier@gmx.de>
2023-04-21 01:06:31 +0200

    feat: add requestHydration to organization store

--- flevents-frontend/src/store/organizations.ts
+++ flevents-frontend/src/store/organizations.ts
@@ -8,11 +8,17 @@ export const useOrganizationStore = defineStore('organizations', {
     managedOrganizations: [] as Organization[],
     loading: false,
     error: false,
+    lastSuccessfulHydration: undefined as Date|undefined,
   }),
   actions: {
+
+    /**
+     * Hydrates the store by requesting the data from the api.
+     */
     async hydrate() {
       this.error = false;
       this.loading = true;
+      await new Promise(resolve => setTimeout(resolve, 1000));
       const account = Security.getAccount();
       if (account === null) {
         console.error('Cannot get events without logged-in user.');
@@ -22,8 +28,8 @@ export const useOrganizationStore = defineStore('organizations', {
       }
       try {
         const response = await AccountApi.getManagedOrganizations(account.uuid);
-        console.log(response);
         this.managedOrganizations = response.data as Organization[];
+        this.lastSuccessfulHydration = new Date();
       } catch (error) {
         console.warn('Failed to fetch events of logged-in user.');
         this.error = true;
@@ -31,6 +37,24 @@ export const useOrganizationStore = defineStore('organizations', {
         this.loading = false;
       }
     },
+
+    /**
+     * Signals the store that recent contents of it's state are being used.
+     * The store then decides whether the last successful hydration is long enough ago to update the state.
+     */
+    async requestHydration() {
+      if (
+        this.lastSuccessfulHydration === undefined
+        || new Date().getTime() - this.lastSuccessfulHydration.getTime() > 60000
+      ) {
+        await this.hydrate();
+      }
+    },
+
+    /**
+     * Retrieves a single organization of all loaded ones.
+     * @param uuid the uuid of the organization
+     */
     getOrganization(uuid: string) {
       return this.managedOrganizations.find(e => e.uuid === uuid);
     },
