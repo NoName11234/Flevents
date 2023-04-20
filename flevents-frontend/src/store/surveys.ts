09d19bcf45665d4f163186e7603f99964e621382
dasdawidt <David_Maier@gmx.de>
2023-04-21 01:08:05 +0200

    feat: create survey store

---
+++ flevents-frontend/src/store/surveys.ts
@@ -0,0 +1,51 @@
+import { defineStore } from 'pinia'
+import surveyApi from "@/api/surveyApi";
+import {Questionnaire} from "@/models/questionnaire";
+
+export const useSurveyStore = defineStore('surveys', {
+  state: () => ({
+    surveys: new Map<string, Questionnaire[]>(),
+    lastSuccessfulHydration: new Map<string, Date>(),
+    loading: false,
+    error: false,
+  }),
+  actions: {
+
+    /**
+     * Hydrates the store by requesting the posts of the given event uuid from the api.
+     * @param eventUuid the uuid of the event associated with requested posts
+     */
+    async hydrate(eventUuid: string) {
+      this.loading = true;
+      this.error = false;
+      // TODO: create and use posts store
+      try {
+        const { data } = await surveyApi.get(eventUuid);
+        this.surveys.set(eventUuid, data as Questionnaire[]);
+        this.lastSuccessfulHydration.set(eventUuid, new Date());
+      } catch (e) {
+        console.warn(`Failed to fetch surveys for event with id ${eventUuid}.`, e);
+        this.error = true;
+      }
+      this.loading = false;
+    },
+
+    /**
+     * Retrieves all questionnaires associated with the event with given uuid.
+     * Initializes the retrieval of the questionnaires if they have not yet been loaded.
+     * @param eventUuid the uuid of the event associated with requested questionnaires
+     * @returns an array of questionnaires
+     */
+    getSurveys(eventUuid: string) {
+      const requestedSurveys = this.surveys.get(eventUuid);
+      const lastUpdate = this.lastSuccessfulHydration.get(eventUuid);
+      if (
+        requestedSurveys === undefined
+        || lastUpdate !== undefined && new Date().getTime() - lastUpdate.getTime() > 60000
+      ) {
+        this.hydrate(eventUuid);
+      }
+      return requestedSurveys || [] as Questionnaire[];
+    },
+  },
+})
