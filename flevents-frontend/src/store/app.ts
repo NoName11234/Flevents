9fd38a962a8df1998202d4663c2677b8b9d02b3b
dasdawidt <David_Maier@gmx.de>
2023-04-21 00:25:00 +0200

    feat: add global state for toasts

--- flevents-frontend/src/store/app.ts
+++ flevents-frontend/src/store/app.ts
@@ -4,5 +4,12 @@ import { defineStore } from 'pinia'
 export const useAppStore = defineStore('app', {
   state: () => ({
     globallyLoading: false,
+    toasts: [] as string[],
   }),
+  actions: {
+    addToast(toastText: string) {
+      this.toasts.push(toastText);
+      setTimeout(() => this.toasts.shift(), 3000);
+    }
+  }
 })
