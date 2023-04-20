e0322a4d905ebf8665c2c52b0c9d20f4a42f2906
dasdawidt <David_Maier@gmx.de>
2023-04-21 01:06:16 +0200

    feat: add requestHydration to event store

--- flevents-frontend/src/store/events.ts
+++ flevents-frontend/src/store/events.ts
@@ -8,10 +8,16 @@ export const useEventStore = defineStore('events', {
     bookedEvents: [] as FleventsEvent[],
     managedEvents: [] as FleventsEvent[],
     exploreEvents: [] as FleventsEvent[],
+    cachedEvents: [] as FleventsEvent[],
     loading: false,
     error: false,
+    lastSuccessfulHydration: undefined as Date|undefined,
   }),
   actions: {
+
+    /**
+     * Hydrates the store by requesting the data from the api.
+     */
     async hydrate() {
       this.error = false;
       this.loading = true;
@@ -28,20 +34,38 @@ export const useEventStore = defineStore('events', {
           AccountApi.getManagedEvents(account.uuid),
           AccountApi.getExploreEvents(account.uuid)
         ]);
-        console.log(responses);
         this.bookedEvents = responses[0].data as FleventsEvent[];
         this.managedEvents = responses[1].data as FleventsEvent[];
         this.exploreEvents = responses[2].data as FleventsEvent[];
-      } catch (error) {
-        console.warn('Failed to fetch events of logged-in user.');
+        this.lastSuccessfulHydration = new Date();
+      } catch (e) {
+        console.warn('Failed to fetch events of logged-in user.', e);
         this.error = true;
       } finally {
         this.loading = false;
       }
     },
+
+    /**
+     * Signals the store that recent contents of it's state are being used.
+     * The store then decides whether the last successful hydration is long enough ago to update the state.
+     */
+    async requestHydration() {
+      if (
+        this.lastSuccessfulHydration === undefined
+        || new Date().getTime() - this.lastSuccessfulHydration.getTime() > 60000
+      ) {
+        await this.hydrate();
+      }
+    },
+
+    /**
+     * Retrieves an event.
+     * @param uuid
+     */
     getEvent(uuid: string) {
-      return this.bookedEvents
-        .concat(this.managedEvents, this.exploreEvents)
+      return this.managedEvents
+        .concat(this.bookedEvents, this.bookedEvents)
         .find(e => e.uuid === uuid);
     },
   },
