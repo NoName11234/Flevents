35d4d51c81cecdcc39cb1ee97bf974e9b6b8fabd
Ruben <ruben.elias.kraft@outlook.de>
2023-04-23 10:06:10 +0200

    feat: add deletion of Account and leave of organization

--- flevents-frontend/src/views/home/Account.vue
+++ flevents-frontend/src/views/home/Account.vue
@@ -1,11 +1,29 @@
 <script setup lang="ts">
+import accountApi from "@/api/accountApi";
 import AccountInfo from "@/components/AccountInfo.vue";
 import Heading from "@/components/Heading.vue";
 import {useAccountStore} from "@/store/account";
 import {storeToRefs} from "pinia";
+import { onMounted, ref } from "vue";
 
 const accountStore = useAccountStore();
+const deleteUser = ref(false); 
 const { currentAccount: account } = storeToRefs(accountStore);
+const selectedOrga = ref();
+const select = ref(false);
+
+function selectOrga(orga : any){
+  selectedOrga.value = orga; 
+  select.value = true; 
+}
+
+
+function deletAcc(){
+
+}
+onMounted(() => {
+  console.log(account.value?.organizationPreviews);
+})
 </script>
 
 <template>
@@ -21,6 +39,13 @@ const { currentAccount: account } = storeToRefs(accountStore);
       icon="mdi-pencil"
       elevation="5"
       :to="{ name: 'accounts.edit' }"
+    />
+    <v-btn
+      class="mt-n8 mr-5"
+      color="red"
+      icon="mdi-delete"
+      elevation="5"
+      @click="deleteUser = true"
     />
 <!--    <v-btn-->
 <!--      class="mt-n8 mr-5"-->
@@ -44,4 +69,34 @@ const { currentAccount: account } = storeToRefs(accountStore);
       </v-btn>
     </v-card-actions>
   </v-card>
+  <v-card>
+    <v-card-title>Ihre Organisationen</v-card-title>
+    <v-card-text>
+      <div v-for="orga in account.organizationPreviews" class="d-flex justify-space-between align-center"><h3>{{orga.name}}</h3> <v-btn  prepend-icon="mdi-account-remove" color="primary" @click="selectOrga(orga)">Verlassen</v-btn></div>
+    </v-card-text>
+  </v-card>
+
+  <v-dialog
+    persistent
+    :model-value="select"
+    class="align-center justify-center"
+    width="auto"
+  >
+    <v-card><v-card-title>Organisation verlassen</v-card-title>
+      <v-card-text>Sind sie sicher das sie die Organisation {{selectedOrga.name}} verlassen möchten?</v-card-text>
+    <v-card-actions><v-btn color="red" prepend-icon="mdi-account-remove" variant="elevated">Verlassen</v-btn><v-btn @click="select = false">Abbrechen</v-btn></v-card-actions></v-card>
+  </v-dialog>
+  
+  <v-dialog
+    persistent
+    :model-value="deleteUser"
+    class="align-center justify-center"
+    width="auto"
+  >
+    <v-card><v-card-title>Account löschen</v-card-title>
+      <v-card-text>Sind sie sicher das sie ihren Account löschen möchten?</v-card-text>
+    <v-card-actions><v-btn color="red" prepend-icon="mdi-delete" variant="elevated" @click="accountApi.delete(account?.uuid as string)">Löschen</v-btn><v-btn @click="deleteUser = false">Abbrechen</v-btn></v-card-actions></v-card>
+  </v-dialog>
+
+
 </template>
