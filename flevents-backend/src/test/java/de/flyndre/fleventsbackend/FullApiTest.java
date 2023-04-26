dc61e57d367942b5b948cd99eebc69f1d91353eb
L4kiLuk <49391930+L4kiLuk@users.noreply.github.com>
2023-04-26 21:38:19 +0200

    add tests

--- flevents-backend/src/test/java/de/flyndre/fleventsbackend/FullApiTest.java
+++ flevents-backend/src/test/java/de/flyndre/fleventsbackend/FullApiTest.java
@@ -1,31 +1,55 @@
 package de.flyndre.fleventsbackend;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import de.flyndre.fleventsbackend.Models.Organization;
-import de.flyndre.fleventsbackend.controller.FleventsAccountController;
-import de.flyndre.fleventsbackend.controller.OrganizationController;
 import de.flyndre.fleventsbackend.dtos.OrganizationInformation;
-import de.flyndre.fleventsbackend.repositories.OrganizationRepository;
+import de.flyndre.fleventsbackend.dtos.OrganizationPreview;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 @TestPropertySource("classpath:application-local.properties")
 public class FullApiTest {
+
     @Autowired
-    private OrganizationController organizationController;
-    @Autowired
-    private OrganizationRepository organizationRepository;
-    @Autowired
-    private FleventsAccountController fleventsAccountController;
+    private MockMvc mockMvc;
+
+    private final ObjectMapper mapper = new JsonMapper();
+
 
 
     @Test
-    void createOrganization(){
+    @WithMockUser(authorities = {"platformAdmin"})
+    void apiTest() throws Exception {
+        OrganizationPreview organizationPreview = new OrganizationPreview();
+        organizationPreview.setName("Test Orga");
+        organizationPreview.setDescription("Test description");
+        var result = mockMvc.perform(MockMvcRequestBuilders.post("/api/platform/organizations")
+                        .param("email","test@flyndre.de")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(organizationPreview)))
+                .andExpect(status().isCreated())
+                .andReturn();
+        OrganizationInformation testOrga = mapper.readValue(result.getResponse().getContentAsString(),OrganizationInformation.class);
 
+
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/platform/organizations/"+testOrga.getUuid()))
+                .andExpect(status().isOk());
     }
 
     public void compare(OrganizationInformation information, Organization organization){
