a6020dc577305104ca6bf7fbbcdbf086580ffbd1
dasdawidt <David_Maier@gmx.de>
2023-04-28 01:44:05 +0200

    feat: edit and delete posts and delete comments

--- flevents-frontend/src/components/Comment.vue
+++ flevents-frontend/src/components/Comment.vue
@@ -2,9 +2,13 @@
 import {Comment} from "@/models/comment";
 import ColorService from "@/service/colorService";
 import DatetimeService from "../service/datetimeService";
-import {computed} from "vue";
+import {computed, ref} from "vue";
 import {storeToRefs} from "pinia";
 import {useAccountStore} from "@/store/account";
+import commentsApi from "@/api/commentsApi";
+import {useEventStore} from "@/store/events";
+import {useAppStore} from "@/store/app";
+import {AxiosError} from "axios";
 
 const props = defineProps({
   comment: {
@@ -16,13 +20,55 @@ const props = defineProps({
     type: Boolean,
     default: false,
   },
+  eventUuid: {
+    required: true,
+    type: String,
+  },
+  postUuid: {
+    required: true,
+    type: String,
+  },
 });
+
+const eventStore = useEventStore();
+const appStore = useAppStore();
+
+const loading = ref(false);
 
 const canDelete = computed(() => {
   if (props.adminView) return true;
   const { currentAccount } = storeToRefs(useAccountStore());
   return currentAccount.value?.uuid === props.comment.author.uuid;
 });
+
+async function deleteComment() {
+  loading.value = true;
+  try {
+    await commentsApi.delete(props.comment?.uuid, props.postUuid, props.eventUuid);
+    appStore.addToast({
+      text: 'Kommentar gelöscht.',
+      color: 'success',
+    });
+    eventStore.hydrateSpecific(props.eventUuid);
+  } catch (e) {
+    let errorMessage = '';
+    if (e instanceof AxiosError) {
+      if (e.code === AxiosError.ERR_BAD_REQUEST) {
+        errorMessage = 'Ungültige Anfrage';
+      }
+      else if (e.code === AxiosError.ERR_NETWORK) {
+        errorMessage = 'Netzwerkfehler';
+      }
+    } else {
+      errorMessage = `Unerwarteter Fehler: ${e}`;
+    }
+    appStore.addToast({
+      text: `Löschen des Kommentars fehlgeschlagen: ${errorMessage}`,
+      color: 'error',
+    });
+  }
+  loading.value = false;
+}
 
 </script>
 
@@ -37,6 +83,8 @@ const canDelete = computed(() => {
       elevation="0"
       border
       class="ms-n4"
+      :loading="loading"
+      :disabled="loading"
     >
       <v-card-text class="d-flex flex-column">
         <div class="d-flex flex-row align-center gap">
@@ -49,6 +97,7 @@ const canDelete = computed(() => {
             icon=""
             size="25"
             variant="text"
+            @click="deleteComment"
           >
             <v-icon
               v-if="canDelete"
