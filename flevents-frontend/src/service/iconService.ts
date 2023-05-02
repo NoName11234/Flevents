462edd20b1a27b68724169b7f1bbd6f499edc489
dasdawidt <David_Maier@gmx.de>
2023-05-02 22:55:45 +0200

    fix: fix regex to match whole extension

--- flevents-frontend/src/service/iconService.ts
+++ flevents-frontend/src/service/iconService.ts
@@ -1,32 +1,32 @@
 const suffixIconMap = new Map<RegExp, string>([
 
   // General
-  [/(png|jpg|jpeg|webp|gif|avif|svg)/, 'mdi-file-image'],
-  [/(mp4|mov|wmv|flv|avi)/, 'mdi-file-video'],
-  [/(mp3|wav|ogg|m4a|flac)/, 'mdi-file-music'],
-  [/(xml|json|yml|c|cpp|cs|html|htm|php|css|scss|js|ts|vue|hs|java|pl|tex|bib|cmd|bat|md)/, 'mdi-file-code'],
-  [/(csv|ssv|tsv)/, 'mdi-file-delimited'],
-  [/(txt)/, 'mdi-file-document'],
+  [/(png|jpg|jpeg|webp|gif|avif|svg)$/, 'mdi-file-image'],
+  [/(mp4|mov|wmv|flv|avi)$/, 'mdi-file-video'],
+  [/(mp3|wav|ogg|m4a|flac)$/, 'mdi-file-music'],
+  [/(xml|json|yml|c|cpp|cs|html|htm|php|css|scss|js|ts|vue|hs|java|pl|tex|bib|cmd|bat|md)$/, 'mdi-file-code'],
+  [/(csv|ssv|tsv)$/, 'mdi-file-delimited'],
+  [/(txt)$/, 'mdi-file-document'],
 
   // PDF
-  [/(pdf)/, 'mdi-file'],
+  [/(pdf)$/, 'mdi-file'],
 
   // OpenDocument
-  [/(odt)/, 'mdi-file-document'],
-  [/(ods)/, 'mdi-file-table'],
-  [/(odp)/, 'mdi-file'],
-  [/(odg)/, 'mdi-file-image'],
+  [/(odt)$/, 'mdi-file-document'],
+  [/(ods)$/, 'mdi-file-table'],
+  [/(odp)$/, 'mdi-file'],
+  [/(odg)$/, 'mdi-file-image'],
 
   // Office
-  [/(doc|docx)/, 'mdi-file-word'],
-  [/(xls|xlsx)/, 'mdi-file-excel'],
-  [/(ppt|pptx)/, 'mdi-file-powerpoint'],
+  [/(doc|docx)$/, 'mdi-file-word'],
+  [/(xls|xlsx)$/, 'mdi-file-excel'],
+  [/(ppt|pptx)$/, 'mdi-file-powerpoint'],
 
   // Archives
-  [/(zip|7zip|7z|gz)/, 'mdi-folder-zip'],
+  [/(zip|7zip|7z|gz)$/, 'mdi-folder-zip'],
 
   // Executables
-  [/(exe|dll|jar|iso)/, 'mdi-file'],
+  [/(exe|dll|jar|iso)$/, 'mdi-file'],
 ]);
 
 /**
