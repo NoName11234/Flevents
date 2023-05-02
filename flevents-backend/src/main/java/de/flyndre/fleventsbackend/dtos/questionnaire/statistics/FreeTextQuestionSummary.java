b835863c3049fc730297158a454b9559e523c959
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2023-05-02 15:10:02 +0200

    dtos updated

--- flevents-backend/src/main/java/de/flyndre/fleventsbackend/dtos/questionnaire/statistics/FreeTextQuestionSummary.java
+++ flevents-backend/src/main/java/de/flyndre/fleventsbackend/dtos/questionnaire/statistics/FreeTextQuestionSummary.java
@@ -3,7 +3,14 @@ package de.flyndre.fleventsbackend.dtos.questionnaire.statistics;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 @Setter
-public class FreeTextQuestionSummary extends QuestionSummary{
+public class FreeTextQuestionSummary implements QuestionSummary{
+    List<String> answers = new ArrayList<>();
+    public FreeTextQuestionSummary(){
+
+    }
 }
--- flevents-backend/src/main/java/de/flyndre/fleventsbackend/dtos/questionnaire/statistics/QuestionSummary.java
+++ flevents-backend/src/main/java/de/flyndre/fleventsbackend/dtos/questionnaire/statistics/QuestionSummary.java
@@ -1,4 +1,12 @@
 package de.flyndre.fleventsbackend.dtos.questionnaire.statistics;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public abstract class QuestionSummary {
+    private enum QuestionType {FreeTextQuestion, SingleChoiceQuestion}
+    private int userCount = 0;
+
 }
