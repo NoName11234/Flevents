baa3cedee2ffca2c4344d6b08125ec846ebd3411
dasdawidt <David_Maier@gmx.de>
2023-05-05 22:14:01 +0200

    fix: improve mobile bottom nav to indicate scroll

--- flevents-frontend/src/layouts/default/MobileNavBar.vue
+++ flevents-frontend/src/layouts/default/MobileNavBar.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 
+import {useDisplay} from "vuetify";
+
 const props = defineProps({
   showPersonal: {
     required: false,
@@ -28,6 +30,8 @@ const props = defineProps({
   }
 });
 
+const { xs: chevronBreakpoint } = useDisplay();
+
 const minWidth = '130px';
 
 </script>
@@ -41,65 +45,77 @@ const minWidth = '130px';
     <div
       class="d-flex flex-row overflow-x-auto"
     >
-    <v-btn
-      v-if="showPersonal"
-      value="personal"
-      size="small"
-      :to="{ name: 'home.personal' }"
-      prepend-icon="mdi-heart"
-      stacked
-      :min-width="minWidth"
-    >
-      <small>Mein Bereich</small>
-    </v-btn>
+      <v-btn
+        v-if="showPersonal"
+        value="personal"
+        size="small"
+        :to="{ name: 'home.personal' }"
+        prepend-icon="mdi-heart"
+        stacked
+        :min-width="minWidth"
+      >
+        <small>Mein Bereich</small>
+      </v-btn>
 
-    <v-btn
-      v-if="showManaged"
-      value="manage"
-      size="small"
-      :to="{ name: 'home.manage' }"
-      prepend-icon="mdi-pencil"
-      stacked
-      :min-width="minWidth"
-    >
-      <small>Verwalten</small>
-    </v-btn>
+      <v-btn
+        v-if="showManaged"
+        value="manage"
+        size="small"
+        :to="{ name: 'home.manage' }"
+        prepend-icon="mdi-pencil"
+        stacked
+        :min-width="minWidth"
+      >
+        <small>Verwalten</small>
+      </v-btn>
 
-    <v-btn
-      v-if="showExplore"
-      value="explore"
-      size="small"
-      :to="{ name: 'home.explore' }"
-      prepend-icon="mdi-compass"
-      stacked
-      :min-width="minWidth"
-    >
-      <small>Entdecken</small>
-    </v-btn>
+      <v-btn
+        v-if="showExplore"
+        value="explore"
+        size="small"
+        :to="{ name: 'home.explore' }"
+        prepend-icon="mdi-compass"
+        stacked
+        :min-width="minWidth"
+      >
+        <small>Entdecken</small>
+      </v-btn>
 
-    <v-btn
-      v-if="showAccount"
-      value="account"
-      size="small"
-      :to="{ name: 'home.account' }"
-      prepend-icon="mdi-account"
-      stacked
-      :min-width="minWidth"
-    >
-      <small>Konto & Info</small>
-    </v-btn>
+      <v-btn
+        v-if="showAccount"
+        value="account"
+        size="small"
+        :to="{ name: 'home.account' }"
+        prepend-icon="mdi-account"
+        stacked
+        :min-width="minWidth"
+      >
+        <small>Konto & Info</small>
+      </v-btn>
 
-    <v-btn
-      v-if="showConsole"
-      value="console"
-      size="small"
-      :to="{ name: 'home.console' }"
-      prepend-icon="mdi-wrench"
-      stacked
-      :min-width="minWidth"
+      <v-btn
+        v-if="showConsole"
+        value="console"
+        size="small"
+        :to="{ name: 'home.console' }"
+        prepend-icon="mdi-wrench"
+        stacked
+        :min-width="minWidth"
+      >
+        <small>Plattformkonsole</small>
+      </v-btn>
+    </div>
+    <div
+      v-if="chevronBreakpoint"
+      class="d-flex flex-row justify-space-between align-center bg-transparent position-absolute h-100 w-100 opacity-50 noclip"
     >
-      <small>Plattformkonsole</small>
-    </v-btn>
+      <v-icon
+        icon="mdi-chevron-left"
+      />
+      <v-spacer />
+      <v-icon
+        icon="mdi-chevron-right"
+      />
     </div>
   </v-bottom-navigation>
 </template>
