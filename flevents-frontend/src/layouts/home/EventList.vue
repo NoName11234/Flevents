187fb6e68416891e7054a8b5bb2cabd269c63110
dasdawidt <David_Maier@gmx.de>
2023-05-07 18:16:19 +0200

    feat: implement filters using filter options

--- flevents-frontend/src/layouts/home/EventList.vue
+++ flevents-frontend/src/layouts/home/EventList.vue
@@ -5,6 +5,10 @@ import EventListFilters from "@/components/EventListFilters.vue";
 import EventCard from "@/components/EventCard.vue";
 import ContentLoadingIndicator from "@/components/ContentLoadingIndicator.vue";
 import CardBanner from "@/components/CardBanner.vue";
+import {useAccountStore} from "@/store/account";
+import {storeToRefs} from "pinia";
+import {EventListFilterOptions} from "@/models/EventListFilterOptions";
+import {EventRole} from "@/models/eventRole";
 
 const props = defineProps({
   events: {
@@ -28,22 +32,62 @@ const props = defineProps({
   }
 });
 
-//TODO: Filtermöglichkeiten (Organisation, Suche, Datumsauswahl, Sortierung), Liste aller verfügbaren Events (die ersten 20 oder so)
+const accountStore = useAccountStore();
+const { currentAccount: account } = storeToRefs(accountStore);
 
-const term = ref('');
+const filters = ref({} as EventListFilterOptions);
+
 const eventList = computed(() => {
-  return props.events?.filter((event) => (
-      event.description?.toLowerCase().includes(term.value)
-      || event.name?.toLowerCase().includes(term.value)
-      || event.location?.toLowerCase().includes(term.value)
-      || event.organizationPreview?.name.toLowerCase().includes(term.value)
-      || event.organizationPreview?.description.toLowerCase().includes(term.value)
-    )
-  ).sort((a, b) => new Date(b.endTime).getTime() - new Date(a.endTime).getTime());
+  const currentFilters = filters.value;
+  let events = [ ...props.events ];
+
+  if (currentFilters.queryText !== undefined) {
+    const queryText = currentFilters.queryText!.toLowerCase();
+    events = events.filter(e =>
+      e.description?.toLowerCase().includes(queryText)
+      || e.name?.toLowerCase().includes(queryText)
+      || e.location?.toLowerCase().includes(queryText)
+      || e.organizationPreview?.name.toLowerCase().includes(queryText)
+      || e.organizationPreview?.description.toLowerCase().includes(queryText)
+    );
+  }
+
+  if (currentFilters.organizationUuids !== undefined && currentFilters.organizationUuids.length > 0) {
+    const organizationUuids = currentFilters.organizationUuids!;
+    events = events.filter(e =>
+      organizationUuids.includes(e.organizationPreview.uuid)
+    );
+  }
+
+  if (currentFilters.eventRoles !== undefined && currentFilters.eventRoles.length > 0) {
+    const eventRoles = currentFilters.eventRoles!;
+    events = events.filter(e =>
+      e.accountPreviews.find(a =>
+        a.uuid === account.value!.uuid
+        && eventRoles.includes(a.role as EventRole)
+      )
+    );
+  }
+
+  if (currentFilters.fromDate !== undefined) {
+    const fromDate = currentFilters.fromDate!;
+    events = events.filter(e =>
+      new Date(e.endTime).getTime() - new Date(fromDate).getTime() >= 0
+    );
+  }
+
+  if (currentFilters.toDate !== undefined) {
+    const toDate = currentFilters.toDate!;
+    events = events.filter(e =>
+      new Date(toDate).getTime() - new Date(e.startTime).getTime() >= 0
+    );
+  }
+
+  return events.sort((a, b) => new Date(b.endTime).getTime() - new Date(a.endTime).getTime());
 });
 
-function updateList(newTerm: string) {
-  term.value = newTerm;
+function updateList(newFilters: EventListFilterOptions) {
+  filters.value = newFilters;
 }
 </script>
 
