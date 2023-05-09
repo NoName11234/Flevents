7a3b94dc8c935e6410b7e75d99c513f2332e99c0
dasdawidt <David_Maier@gmx.de>
2023-05-09 22:28:17 +0200

    fix: tsdocs

--- flevents-frontend/src/service/datetimeService.ts
+++ flevents-frontend/src/service/datetimeService.ts
@@ -1,21 +1,37 @@
 import {VLocaleProvider} from "vuetify/components";
 
-
 class DatetimeService {
+
   locale = "DE-de";
+
   constructor(locale: string) {
     this.locale = locale;
   }
+
+  /**
+   * Gets the date part of a date as formatted string.
+   * @param date the date
+   */
   getDate(date: Date) {
     return date.toLocaleDateString(this.locale, {
       dateStyle: 'medium'
     });
   }
+
+  /**
+   * Gets the time part of a date as formatted string.
+   * @param date the date
+   */
   getTime(date: Date) {
     return date.toLocaleTimeString(this.locale, {
       timeStyle: 'short'
     });
   }
+
+  /**
+   * Gets both, date and time part, of a date as formatted string.
+   * @param date the date
+   */
   getDateTime(date: Date) {
     return date.toLocaleString(this.locale, {
       dateStyle: 'medium',
@@ -23,6 +39,12 @@ class DatetimeService {
     });
   }
 
+  /**
+   * Formats a date string using two dates.
+   * If both are on the same day, the second date's dd.MM.YYYY is omitted.
+   * @param from the starting date string
+   * @param to the ending date string
+   */
   formatDateRange(from: any, to: any) {
     let short = {
       timeStyle: "short",
@@ -34,13 +56,13 @@ class DatetimeService {
     let start = new Date(from);
     let end = new Date(to);
     if (this.isSameDay(start, end)) {
-      return start.toLocaleString("DE-de", long)
+      return start.toLocaleString(this.locale, long)
         + " - "
-        + end.toLocaleString("DE-de", short);
+        + end.toLocaleString(this.locale, short);
     }
-    return start.toLocaleString("DE-de", long)
+    return start.toLocaleString(this.locale, long)
       + " - "
-      + end.toLocaleString("DE-de", long);
+      + end.toLocaleString(this.locale, long);
   }
 
   /**
